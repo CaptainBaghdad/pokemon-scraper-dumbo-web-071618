7a9b6f60629473614d6f6013690cf1070e2d3c49
Jason Pitts <juniorjr083@gmail.com>
2018-07-24 21:07:24 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,7 +26,7 @@ class Pokemon
     ans = db.execute(sql,id)
     ans = ans.flatten
     
-    pokey = Pokemon.new(id: ans[0], name: ans[1], type: ans[2], db: db)
+    pokey = Pokemon.new(id: ans[0], name: ans[1], type: ans[2], db: db, hp: ans[3])
     return pokey
    
    
