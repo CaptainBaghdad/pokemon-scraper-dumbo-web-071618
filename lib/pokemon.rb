f744d536af18727f25e413bc5238dbb47c6da594
Jason Pitts <juniorjr083@gmail.com>
2018-07-24 18:37:52 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
 
  #attr_accessor :name, :type
  
- def initialize(id:, name:, type:, db:)
+ def initialize(id: nil, name: nil, type: nil, db: nil)
     @id = id
     @name = name 
     @type = type
