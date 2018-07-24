cf21fb9335ffc25128f3747741b39bce9653ae3b
Jason Pitts <juniorjr083@gmail.com>
2018-07-24 19:01:08 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,7 @@ class Pokemon
  end
  
  def self.find(id,db)
-   sql  = "SELECT * FROM pokemon WHERE pokemon[0] = ?;"
+   sql  = "SELECT * FROM pokemon WHERE pokemon[:id] = ?;"
    ans = db.execute(sql,id)
    
    
